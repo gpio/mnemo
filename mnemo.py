b248735a0e8cb71924e31ce61f71b6dca3eb404e
gpio <fmorand@live.com>
2018-08-07 19:47:58 +0200

    Initial commit.

---
+++ mnemo.py
@@ -0,0 +1,10 @@
+#!env /bin/python
+import sys, os
+d = {"0": "(ce|ci|[zsç])", "1": "[td]", "2": "n", "3": "m", "4": "r","5": "l", "6": "(ch|ge|gé|gè|gê|gi|j)", "7": "(ga|geu|go|gu|ca|co|cu|[kq])", "8": "(ph|[fv])", "9": "[bp]"}
+x = "^[haàâeéèêëiïoôöuùûy]*"
+for c in sys.argv[1]:
+    if c in d:
+        x += d[c] + "[aàâeéèêëiïoôöuùûy]*"
+    else:
+        x += c
+os.system('egrep "'+x+'$" ~/bin/dico.txt')
