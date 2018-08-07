5003808a9ff4738df1af49f84c16562c519a2ee9
gpio <fmorand@live.com>
2018-08-07 21:01:11 +0200

    Update mnemo.py

--- mnemo.py
+++ mnemo.py
@@ -1,6 +1,6 @@
 #!env /bin/python
 import sys, os
-d = {"0": "(ce|ci|[zsç])", "1": "[td]", "2": "n", "3": "m", "4": "r","5": "l", "6": "(ch|ge|gé|gè|gê|gi|j)", "7": "(ga|geu|go|gu|ca|co|cu|[kq])", "8": "(ph|[fv])", "9": "[bp]"}
+d = {"0": "(ce|ci|[zsç])", "1": "[td]", "2": "(n|gn)", "3": "m", "4": "r","5": "l", "6": "(ch|ge|gé|gè|gê|gi|j)", "7": "(ga|geu|go|gu|ca|co|cu|[kq])", "8": "(ph|[fv])", "9": "[bp]"}
 x = "^[haàâeéèêëiïoôöuùûy]*"
 for c in sys.argv[1]:
     if c in d:
